1bf06f896443fc2092f7cd9f85cd197c7fbfb8bf
JV-ROCHA <joao.rocha5563@gmail.com>
2026-06-24 18:18:09 -0300

    add class DeleteCommand

--- Command.py
+++ Command.py
@@ -22,3 +22,14 @@ class NewCommand(Command):
         db[id_pessoa] = Pessoa(id_pessoa, nome)
         print(f"Pessoa criada: ID={id_pessoa}, Nome={nome}")
 
+class DeleteCommand(Command):
+    def execute(self, args: list, db: dict):
+        if len(args) < 2:
+            print("Erro: Comando 'delete' requer ID.")
+            return
+        id_pessoa = int(args[1])
+        if id_pessoa in db:
+            del db[id_pessoa]
+            print(f"Pessoa com ID={id_pessoa} deletada.")
+        else:
+            print(f"Erro: Pessoa com ID={id_pessoa} não encontrada.")
